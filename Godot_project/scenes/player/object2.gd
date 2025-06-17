637c8f919573d13970524c9b9e77b655e9e1d0a8
priyanshudebnath <38852086+priyanshudebnath@users.noreply.github.com>
2025-06-17 12:42:33 +0530

    Sonar fix

--- Godot_project/scenes/player/object2.gd
+++ Godot_project/scenes/player/object2.gd
@@ -14,15 +14,16 @@ class_name echo_location
 @export var cone_angle := 30.0             # Half angle of cone
 @export var echoOrigin : Vector2
 
-var reverseEcho : bool = false
+var _reverseEcho : bool = false
+var _fade : float = 1.0
+var _radius : float 
 
 func _ready() -> void:
 	max_radius = max(get_viewport_rect().size.x ,get_viewport_rect().size.y) * 2
-	# For reverse direction: point away from screen center
-	#var center := viewport_size / 2.0
-	#var direction := (get_viewport().get_mouse_position() - center).normalized()
+
 	var direction := (get_viewport().get_mouse_position() - Global.PlayerPos).normalized()
 	mat.set_shader_parameter("direction", direction)
+	echo_timer.stop()
 	echo_timer.start()
 	_connect_signals()
 	
@@ -30,32 +31,32 @@ func _connect_signals() -> void:
 	echo_timer.connect("timeout",_echo_timeout)
 
 func _echo_timeout() -> void:
-	reverseEcho = 1
-
-
+	_reverseEcho = 1
 	
 
 func _process(delta: float) -> void:
 	pulse_time += delta
-	if reverseEcho:
-		mat.set_shader_parameter("hide", 1.0)
-	else:
-		mat.set_shader_parameter("hide", 1.0)
-	
+	if _reverseEcho:
+		_fade -= delta
+		mat.set_shader_parameter("fade", _fade)
+		if _fade <= 0.0:
+			echo_timer.stop()
+			queue_free()
+		
+	else: 
+		_radius = lerp(min_radius, max_radius, pulse_time/pulse_duration)
+		mat.set_shader_parameter("fade", _fade)
+		
 	# Animate the radius
-	var radius :Variant= lerp(min_radius, max_radius, pulse_time/pulse_duration)
-	radius = clampf(radius, min_radius, max_radius)
-	mat.set_shader_parameter("radius", radius)
+	
+	_radius = clampf(_radius, min_radius, max_radius)
+	mat.set_shader_parameter("radius", _radius)
 	mat.set_shader_parameter("softness", softness)
 	mat.set_shader_parameter("angle", cone_angle)
 	
 	#printt(radius, pulse_time/pulse_duration)
-	
-	# Get the mouse position and convert to UV
-	
-	#mouse_pos = get_viewport().get_mouse_position() - Vector2(20.0, 20.0)
-	#mouse_pos = Vector2(0.5,0.5) - Vector2(20.0, 20.0)
+
 	var origin_uv := echoOrigin / viewport_size
-	#printt(origin_uv, viewport_size, mouse_pos)
+
 
 	mat.set_shader_parameter("reveal_origin", origin_uv)
