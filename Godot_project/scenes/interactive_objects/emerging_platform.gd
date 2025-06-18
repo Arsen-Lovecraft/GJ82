76b94b95985ef0b2e712a73f02b4deb641be5628
priyanshud <priyanshud@quickcap.net>
2025-06-18 22:21:44 +0530

    Platform calculations

--- Godot_project/scenes/interactive_objects/emerging_platform.gd
+++ Godot_project/scenes/interactive_objects/emerging_platform.gd
@@ -5,8 +5,11 @@ extends StaticBody2D
 @onready var collision_shape := $CollisionShape2D  # Or use get_node() to reach the Polygon2D
 
 var _emit_to_platform_verti_distances: Array
+var _emit_to_platform_verti_angles: Array
 var _emit_to_platform_angle_deg: float
 var _platform_reveal_duration : float
+var _reveal_time_speed_scale: float = 1.0
+var _disappear_time_speed_scale:float = 1.0
 
 
 @onready var _seen_timer: Timer = %SeenTimer
@@ -26,13 +29,17 @@ func _connect_signals() -> void:
 func _on_sonar_emmission (_emit_position:Vector2, size_relative_to_radius:float, reveal_time: float, disappear_time: float, angle_in_rad: float) -> void:
 	#printt(_emit_position,size_relative_to_radius,reveal_time,disappear_time,angle_in_rad)
 	_sonar_vs_platform_calculations(_emit_position, size_relative_to_radius, reveal_time, disappear_time, angle_in_rad)
-
-func _sonar_vs_platform_calculations(_emit_position:Vector2, size_relative_to_radius:float, reveal_time: float, disappear_time: float, angle_in_rad: float) -> void:
-	get_global_vertices(_emit_position)
-	print(_emit_to_platform_verti_distances)
+	_reveal_time_speed_scale = 1.0/reveal_time
+	_disappear_time_speed_scale = 1.0/disappear_time
+	printt(_emit_to_platform_verti_distances, _emit_to_platform_verti_angles, \
+	_reveal_time_speed_scale, _disappear_time_speed_scale)
 	
-
-func get_global_vertices(_emit_position: Vector2) -> void:
+	
+func _sonar_vs_platform_calculations(_emit_position:Vector2, size_relative_to_radius:float, reveal_time: float, disappear_time: float, angle_in_rad: float) -> void:
+	get_platform_global_data(_emit_position)
+	
+	
+func get_platform_global_data(_emit_position: Vector2) -> void:
 	var shape := collision_shape.shape as RectangleShape2D
 	var extents :Variant= shape.extents
 	var local_points := [
@@ -41,11 +48,12 @@ func get_global_vertices(_emit_position: Vector2) -> void:
 		Vector2(extents.x, extents.y),   #BottomRight
 		Vector2(-extents.x, extents.y),  #BottomLeft
 	]
-
-	var global_points := []
+	_emit_to_platform_verti_distances = []
+	_emit_to_platform_verti_angles = []
 	for p:Variant in local_points:
-		global_points.append(_emit_position.distance_to(collision_shape.to_global(p)))
-	_emit_to_platform_verti_distances = global_points
+		_emit_to_platform_verti_distances.append(_emit_position.distance_to(collision_shape.to_global(p)))
+		_emit_to_platform_verti_angles.append(_emit_position.angle_to_point(collision_shape.to_global(p)))
+
 
 
 func _emerge() -> void:
--- Godot_project/scripts/level.gd
+++ Godot_project/scripts/level.gd
@@ -3,6 +3,10 @@ extends Node2D
 
 var _settings_ps: PackedScene = preload("uid://ldg2jq7gg87x")
 
+var _player_to_platform_angles : Array
+
+
 func _process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("ui_cancel")):
 		get_tree().current_scene.add_child(_settings_ps.instantiate())
+	
