ef878db5cd3294b15a71634df549719f9ebe7e1e
Arsen-Lovecraft <50301289+Arsen-Lovecraft@users.noreply.github.com>
2025-06-18 04:28:05 +0300

    Add a level with a button and a door + add sonar emitting mechanic to player
    Add first playable level
    Add "A button and a door" behaviour code to the character script
    Export sonar variable in the character script
    Deleted useless shaders
    Add static environment colliders and new physic layer for static environment(second layer)

--- Godot_project/scenes/player/player.gd
+++ Godot_project/scenes/player/player.gd
@@ -3,6 +3,13 @@ class_name Player
 
 signal gameOver
 
+@export_group("Sonar")
+@export var reveal_time: float = 1.0
+@export var disappear_time: float = 1.0
+@export var revealing_time: float = 1.0
+@export var size_relative_to_radius: float = 500
+@export_group("Other")
+
 var _sonar_scene_ps: PackedScene = preload("uid://1yg762auekjc")
 
 @onready var player_sprite: AnimatedSprite2D = %playerSprite
@@ -10,6 +17,7 @@ var _sonar_scene_ps: PackedScene = preload("uid://1yg762auekjc")
 @onready var echo_sound: AudioStreamPlayer = %echoSound
 @onready var jump_sound: AudioStreamPlayer = %jumpSound
 @onready var _echo_pos: Marker2D = %echoPos
+@onready var _interaction_area: Area2D = %InteractionArea
 
 
 @export var _player_data : RplayerData = preload("uid://byrd0re6gadg6")
@@ -31,7 +39,7 @@ func _ready() -> void:
 	_init_data()
 
 func _connect_signals()->void:
-	pass
+	_interaction_area.area_entered.connect(_on_interaction_area_entered)
 
 func _init_data() -> void:
 	speed = _player_data.speed
@@ -39,7 +47,7 @@ func _init_data() -> void:
 
 func _on_body_entered(_body: Variant) -> void:
 	pass
-	
+
 func _physics_process(delta: float) -> void:
 	Global.PlayerPos = global_position
 	
@@ -102,16 +110,24 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("sonar"):
 		var sonar: Sonar = _sonar_scene_ps.instantiate()
 		get_tree().current_scene.add_child(sonar)
-		sonar.set_sonar_parametres(1.0,1.0)
-		sonar.emit_sonar(_echo_pos.global_position, get_global_mouse_position().angle() + PI/2)
-	
+		sonar.set_sonar_parametres(reveal_time,disappear_time,revealing_time,size_relative_to_radius)
+		sonar.emit_sonar(_echo_pos.global_position, _echo_pos.get_angle_to(get_global_mouse_position()) + PI/2 )
+	if(Input.is_action_just_pressed("interact")):
+		_try_to_activate_button()
 	
 	if not direction:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	move_and_slide()
-
 ## _physics_process END
-	
+
+func _try_to_activate_button() -> void:
+	for i: Area2D in _interaction_area.get_overlapping_areas():
+		if(i is InteractiveButton):
+			(i as InteractiveButton).activate_button()
+
+func _on_interaction_area_entered(area: Area2D) -> void:
+	if(area is InteractiveDoor):
+		SceneManager.load_scene((area as InteractiveDoor).level_to_load)
 
 func play_animation(anim_name: String) -> void:
 	if player_sprite.animation != anim_name:
