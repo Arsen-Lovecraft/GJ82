f044d9b217d9d7b41811b61339f91aac639b80f4
Arsen-Lovecraft <50301289+Arsen-Lovecraft@users.noreply.github.com>
2025-06-22 20:34:06 +0300

    cutscene_integration

--- Godot_project/scenes/settings/settings_ui.gd
+++ Godot_project/scenes/settings/settings_ui.gd
@@ -2,7 +2,6 @@ class_name SettingsWindows
 extends CanvasLayer
 
 @onready var _game: GameSettingsUI = %GAME
-@onready var _exit_button: Button = %ExitButton
 var _prev_mouse_capture_mode: Input.MouseMode
 
 func _ready() -> void:
@@ -12,16 +11,9 @@ func _ready() -> void:
 	get_tree().paused = true
 
 func _connect_signals() -> void:
-	if _exit_button.pressed.connect(_on_pressed_exited): printerr("Fail: ",get_stack())
 	if _game.game_continued.connect(_on_continued_pressed): printerr("Fail: ",get_stack())
 
 func _on_continued_pressed() -> void:
 	get_tree().paused = false
 	Input.mouse_mode = _prev_mouse_capture_mode
-	queue_free()
-
-func _on_pressed_exited() -> void:
-	get_tree().paused = false
-	Input.mouse_mode = _prev_mouse_capture_mode
-	get_tree().quit(0)
 	queue_free()
